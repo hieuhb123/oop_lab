1cf12871db2bc7e86770e0327a602001e4d2ba18
hieuhb123 <sieukhoe654321@gmail.com>
2024-03-15 14:52:15 +0700

    1

--- lab1/choosingoption.java
+++ lab1/choosingoption.java
@@ -2,11 +2,9 @@ import javax.swing.JOptionPane;
 
 public class choosingoption {
     public static void main(String[] args) {
-        int option = JOptionPane.showConfirmDialog(null,"Yes", 
-        "No", JOptionPane.YES_NO_OPTION);
+        int option = JOptionPane.showConfirmDialog(null,"Yes", "No", JOptionPane.YES_NO_OPTION);
 
-        JOptionPane.showMessageDialog(null, "You've chosen: "
-        + (option==JOptionPane.YES_OPTION?"Yes":"No"));
+        JOptionPane.showMessageDialog(null, "You've chosen: " + (option==JOptionPane.YES_OPTION?"Yes":"No"));
 
         System.exit(0);
     }
