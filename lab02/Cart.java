b8bb83d38867a1565b951390fcfa370a6c668771
hieuhb123 <sieukhoe654321@gmail.com>
2024-03-25 13:14:52 +0700

    1

---
+++ lab02/Cart.java
@@ -0,0 +1,33 @@
+package lab02;
+
+public class Cart {
+    public static final int MAX_NUMBER_ODERED = 20;
+    private DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBER_ODERED];
+    private int qtyOrdered;
+    
+    public Cart(){
+        qtyOrdered = 0;
+    }
+    
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc){
+        itemOrdered[0] = disc;
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){
+        if(this.qtyOrdered < MAX_NUMBER_ODERED){
+            itemOrdered[qtyOrdered] = disc;
+            this.qtyOrdered++;
+        }
+        else 
+            removeDigitalVideoDisc(disc);
+        System.out.println("The disc has been added");
+        if(qtyOrdered >= MAX_NUMBER_ODERED - 2) System.out.println("The cart is almost full");
+    }
+
+    public double totalCost(){
+        double total = 0;
+        for(int i = 0 ; i < qtyOrdered ; i++)
+            total = total + itemOrdered[i].getCost();
+        return total;
+    }
+}
